2ff385aa3bbc512a5d20110d9aa1bbe770ddcdad
ArthurJuwer <arthur.juwer99@gmail.com>
2025-09-15 17:01:43 -0300

    foi adicionado novas funcionalidades, dentre elas 1-melhora no design das caracteristicas dos personagens, 2- identificacao com cores de certo ou errado, 3- melhora na estrutura das funcoes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const formCharacter = document.querySelector("#formCharacter")
-
+let cartaPrincipal = {}
 
 
 formCharacter.addEventListener("submit", async (e) => {
@@ -17,37 +17,120 @@ formCharacter.addEventListener("submit", async (e) => {
 
     // const data = await response.json()
     // alert(`Sucesso no Data - ${data.name}`)
-    carregarResposta()
+    mostrarCartaResposta()
 
 
 })
 
+
 async function carregarResposta() {
     const response = await fetch("http://localhost:3000/clashroyale")
-    const usuarios = await response.json()
-
-    const lista = document.querySelector("#listaCharacter")
-
-    console.log(usuarios)
+    const todasCartas = await response.json()
+    return todasCartas
     
-    let name = document.querySelector("#name").value
-
-    usuarios.forEach(carta => {
-        console.log("entrou")
-        console.log(name)
-        if(name == carta.nome){
-            console.log("entrou 2")
-            let li = document.createElement("li")
-            li.textContent = `${carta.nome} - ${carta.genero} | ${carta.tipo} | ${carta.raridade} | ${carta.elixir} | ${carta.lancamento} | ${carta.arena} | ${carta.ataque}`
-            lista.appendChild(li)
-        }
-    });
-
-
-    // usuarios.forEach(carta => {
-    //     const li = document.createElement("li");
-    //     li.textContent = `${carta.nome} - ${carta.genero} | ${carta.tipo} | ${carta.raridade} | ${carta.elixir} | ${carta.lancamento} | ${carta.arena} | ${carta.ataque}`
-    //     lista.appendChild(li)
-    // });
 }
 
+async function mostrarCartaResposta() {
+    const lista = document.querySelector("#listaCharacter")
+    const todasCartas = await carregarResposta()
+    let inputName = document.querySelector("#name")
+    let name = inputName.value
+
+    for (const carta of todasCartas) {
+        if (name.toUpperCase() === carta.nome.toUpperCase()) {
+
+            const atributosCorretos = await verificarCaracteristicasCartaPrincipalCartaResposta(carta)
+
+            let ul = document.createElement("ul")
+            ul.classList.add("flex", "gap-10")
+
+            const atributos = ["genero", "tipo", "raridade", "elixir", "lancamento", "arena", "ataque"]
+
+            atributos.forEach(attr => {
+                let li = document.createElement("li")
+                li.classList.add("size-24", "flex", "items-center", "justify-center", "border-yellow-500", "border-2", "text-white", "font-semibold", "relative", "text-center")
+
+                // Criar span para o texto com z-index alto
+                let texto = document.createElement("span")
+                texto.classList.add("relative", "z-10")
+                texto.textContent = carta[attr]
+                li.appendChild(texto)
+
+                if (atributosCorretos.includes(attr)) {
+                    li.classList.add("bg-green-800")
+                } else {
+                    li.classList.add("bg-red-800")
+                    let rotate;
+
+                    // Adicionar seta atrás do texto somente se for "lancamento" maior
+                    if(attr == "lancamento" && carta.lancamento ){
+                    // Colocar o texto em um span com z-index alto
+                    if(carta.lancamento > cartaPrincipal.lancamento){
+                        rotate = "rotate-90 mb-2.5"
+                    } else {
+                        rotate = "-rotate-90 mt-2.5"
+                    }
+
+                    li.innerHTML = `<span class="relative z-10">${carta[attr]}</span>
+                    <div class="absolute left-1/2 -translate-x-1/2  w-16 h-3 bg-black ${rotate} z-0">
+                        <!-- Cabo da seta -->
+                        <div class="absolute top-1/2 -translate-y-1/2 left-0 w-full h-3 bg-black"></div>
+                        
+                        <!-- Triângulo da ponta -->
+                        <div class="absolute top-1/2 -translate-y-1/2 -right-3 w-0 h-0 border-l-12 border-l-black border-y-12 border-y-transparent"></div>
+                    </div>`;
+                }
+                }
+
+                ul.appendChild(li)
+            })
+
+            lista.appendChild(ul)
+        }
+    }
+
+    verificarCartaPrincipalCartaResposta(name)
+    inputName.value = ""
+}
+
+
+async function carregarCartaPrincipal() {
+
+    const todasCartas = await carregarResposta()
+
+    const numeroAleatorio = Math.floor(Math.random() * todasCartas.length + 1)
+    console.log(numeroAleatorio)
+
+    todasCartas.forEach(carta => {
+            if(carta.id == numeroAleatorio){
+                cartaPrincipal = carta
+                console.log(cartaPrincipal)
+                
+            }
+    });
+
+}
+
+async function verificarCartaPrincipalCartaResposta(resposta) {
+    
+    if(resposta.toUpperCase() === cartaPrincipal.nome.toUpperCase()){
+        alert("ACERTOU")
+    }
+    
+}
+
+async function verificarCaracteristicasCartaPrincipalCartaResposta(cartaAtual) {
+        const atributos = ["genero", "tipo", "raridade", "elixir", "lancamento", "arena", "ataque"]
+        const atributosCorretos = []
+
+        atributos.forEach(attr => {
+            if(cartaAtual[attr] === cartaPrincipal[attr]) {
+                atributosCorretos.push(attr)
+            }
+        }) 
+        // console.log(atributosCorretos)
+        return atributosCorretos
+}
+
+carregarResposta()
+carregarCartaPrincipal()
